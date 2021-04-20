746d3cf94fa74ef5532c353712388b930f072924
Jakelfrench21 <77357695+Jakelfrench21@users.noreply.github.com>
2021-04-20 18:57:24 -0400

    Update Welcome.php

--- Welcome.php
+++ Welcome.php
@@ -1,9 +1,7 @@
 <html>
-
-    <body>
-
-    <div class=welcome>
-        <?php
+	<body>
+		<div class=welcome>
+			<?php
             function isAdmin($user_data) {
                 return isset($user_data) && isset($user_data['role']) && $user_data['role'] == 'Admin';
                 }
@@ -23,31 +21,33 @@
                 {
                     echo '<a href="/php/create_account.php"><input type=submit value="Create Account"></a>';
                     echo '<a href="/php/admin.php"><input type=submit value=Administration name=adminpage></a>';
+					echo '<a href="Agenda.php"><input type=submit value="Agenda"></a>';
+					// echo '<a href="logout.php"><input type=submit value=Logout></a>';
                 }
                 if (isStudent($_SESSION)) 
                 {
                     echo '<a href="/php/cashier.php"><input type=submit value="POS Service"></a>';
-					echo "<a href='Agenda.php'>Agenda</a>";
+					echo '<a href="Agenda.php"><input type=submit value="Agenda"></a>';
+					 //echo '<a href="logout.php"><input type=submit value=Logout></a>';
                 }
                   if (isParent($_SESSION)) 
                 {
-					
                 echo '<a href="child.html"><input type=submit value="Child Info"></a>';
                     echo '<a href="Grades.html"><input type=submit value="Grades"></a>';
 			   echo '<a href="Classes.html"><input type=submit value="Classes"></a>';
-			   echo '<a href="/logout.php"><input type=submit value=Logout></a>';
+			  echo '<a href="Agenda.php"><input type=submit value="Agenda"></a>';
+			   //echo '<a href="logout.php"><input type=submit value=Logout></a>';
 				}
-                          
-	    
+				echo '<a href="logout.php"><input type=submit value=Logout></a>';
+			
 	    } 
+			
             else 
             {
                 echo "Not logged in";
                 // header('Location:/login.php');
             }
         ?>
-    </div>
-
-    </body>
-
+</div>
+	</body>
 </html>
