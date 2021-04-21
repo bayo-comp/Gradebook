ca6abfef082d9892ff327451d98ca7a56829def5
catcul7 <catcul7@gmail.com>
2021-04-21 10:40:19 -0400

    Updating format
    Changing Table call format to match other formats

---
+++ Admin.php
@@ -0,0 +1,39 @@
+<?php
+	session_start();
+	$conn = mysqli_connect('localhost','root','','projectDB');
+	if(!$conn){
+		die('Connection Failed! Error: ' . mysqli_error($conn));
+	}
+	echo '<a href="Welcome.php"><input type=submit value=Welcome Info name=welcomepage></a>';
+	echo " ";
+	echo '<a href="Login.php"><input type=submit value=Login Info name=loginpage></a>';
+	echo "<h1>Admins</h1>";
+	echo "<h2>Here is the list of Admins:</h2>";
+	echo "<table border='1' >
+	<tr>
+	<td align=center> <b> First Name </b></td>
+	<td align=center><b> Last Name </b></td>
+	<td align=center><b> Gender </b></td>
+	<td align=center><b> School Name </b></td>
+	</tr>";
+	
+	$username  = $_SESSION['username'];
+	$sqlp="SELECT firstName, lastName, Gender, SchoolName FROM admin";
+	$result = $conn->query($sqlp);
+	//the problem was the query code didn't work for some reason.
+	if ($result && $result->num_rows > 0) {
+	// output data of each row
+	while($row = $result->fetch_assoc()) {
+		echo"<tr>";
+		echo "<td align=center> " .$row["firstName"]."<br></td>";
+		echo "<td align=center> " .$row["lastName"]."<br></td>";
+		echo "<td align=center> " .$row["Gender"]."<br></td>";
+		echo "<td align=center> " .$row["SchoolName"]."<br></td>";
+		//place in each table. 
+			//figure out the the problem to the code above. 
+	}
+	} else {
+	echo "0 results";
+	}
+	$conn->close();
+?>
---
+++ sTable.php
@@ -0,0 +1,43 @@
+<?php
+	session_start();
+	$conn = mysqli_connect('localhost','root','','projectDB');
+	if(!$conn){
+		die('Connection Failed! Error: ' . mysqli_error($conn));
+	}
+	echo '<a href="Welcome.php"><input type=submit value=Welcome Info name=welcomepage></a>';
+	echo " ";
+	echo '<a href="Login.php"><input type=submit value=Login Info name=loginpage></a>';
+	echo "<h1>Students</h1>";
+	echo "<h2>Here is the list of Students:</h2>";
+	echo "<table border='1' >
+	<tr>
+	<td align=center> <b> First Name </b></td>
+	<td align=center><b> Last Name </b></td>
+	<td align=center><b> Gender </b></td>
+	<td align=center><b> Mother's Name </b></td>
+	<td align=center><b> Father's Name </b></td>
+	<td align=center><b> Teacher's Name </b></td>
+	</tr>";
+	
+	$username  = $_SESSION['username'];
+	$sqlp="SELECT firstName, lastName, Gender, MotherUsername, FatherUsername, TeacherUsername FROM student";
+	$result = $conn->query($sqlp);
+	//the problem was the query code didn't work for some reason.
+	if ($result && $result->num_rows > 0) {
+	// output data of each row
+	while($row = $result->fetch_assoc()) {
+		echo"<tr>";
+		echo "<td align=center> " .$row["firstName"]."<br></td>";
+		echo "<td align=center> " .$row["lastName"]."<br></td>";
+		echo "<td align=center> " .$row["Gender"]."<br></td>";
+		echo "<td align=center> " .$row["MotherUsername"]."<br></td>";
+		echo "<td align=center> " .$row["FatherUsername"]."<br></td>";
+		echo "<td align=center> " .$row["TeacherUsername"]."<br></td>";
+		//place in each table. 
+			//figure out the the problem to the code above. 
+	}
+	} else {
+	echo "0 results";
+	}
+	$conn->close();
+?>
