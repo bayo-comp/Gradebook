41ac9fbd358a28e78e23701437865693bd7f6ca9
Anthony Pierce <Ant.pierce24@gmail.com>
2021-04-21 23:26:52 -0400

    Update config.php

--- config.php
+++ config.php
@@ -11,4 +11,12 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+
+function logout($desired_location) 
+{
+    session_start();
+    session_unset();
+    session_destroy();
+    header($desired_location);
+}
 ?>
