1a4fa62f6b4466847fd9cd800c54d239fedbdcd3
catcul7 <catcul7@gmail.com>
2021-04-27 12:36:42 -0400

    Update Admin.php

--- Admin.php
+++ Admin.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	$conn = mysqli_connect('localhost','root','','webdesigndb');
+	$conn = mysqli_connect('localhost','root','','webdesign');
 	if(!$conn){
 		die('Connection Failed! Error: ' . mysqli_error($conn));
 	}
