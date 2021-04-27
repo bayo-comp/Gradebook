f7a61b25e8f32bec251fa298cd32f398bf2dcfe2
bayo-comp <54155076+bayo-comp@users.noreply.github.com>
2021-04-27 10:54:22 -0400

    Update Welcome.php

--- Welcome.php
+++ Welcome.php
@@ -29,7 +29,7 @@
 					echo '<a href="sTable.php"><input type=submit value=Student Info name=studentpage></a>';
 					echo '<a href="Register.php"><input type=submit value=Register Info name=regpage></a>';
                 }
-				if (isTeacher($_SESSION)) 
+	if (isTeacher($_SESSION)) 
                 {
                     echo '<a href="/create_account.php"><input type=submit value="Create Account"></a>';
                     echo '<a href="/teacher.php"><input type=submit value="View Students" name=teacherpage></a>';
@@ -39,9 +39,47 @@
                 }
                 if (isStudent($_SESSION)) 
                 {
-                    echo '<a href="Grades.html"><input type=submit value="Grades"></a>';
-					echo '<a href="Classes.html"><input type=submit value="Classes"></a>';
-					echo '<a href="Agenda.php"><input type=submit value="Agenda"></a>';
+		        <link rel="stylesheet" href="Style.css">
+                    <body>
+                    <div id="page-wrap">
+                    <div class="student-profile py-4">
+                    <div class="center-container">
+                      <div class="row">
+                        <div class="col-lg-4">
+                          <div class="card shadow-sm">
+                            <div class="card-header bg-transparent text-center" style="text-align:center">
+                              <img class="profile_img" src="https://source.unsplash.com/500x200/?student" alt="Avatar" style="border-radius: 50%">
+                              <h3 style="text-align:center">',$_SESSION["username"],'</h3>
+                            </div>
+                            <div class="card-body" style="text-align:center">
+                              <p class="mb-0"><strong class="pr-1">Student ID:</strong>',$_SESSION["id"],'</p>
+                              <p class="mb-0"><strong class="pr-1">Class:</strong>',$_SESSION["NumberofClasses"],'</p>
+                              <p class="mb-0"><strong class="pr-1">Section:</strong>A</p>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-lg-8">
+                          <div class="card shadow-sm">
+                            <div class="card-header bg-transparent border-0" style="text-align:center">
+                              <h3 class="mb-0"><i class="far fa-clone pr-1"></i>General Information</h3>
+                            </div>
+                            <div class="card-body pt-0" style="text-align:center">
+                            <span><label style="font-weight:bold;">Academic Year: </label><p>2021</p></span>
+                            </div>
+                          </div>
+                            <div style="height: 20px"></div>
+                            <div class="card shadow-sm" style="text-align:center">
+                            <a href="/StudentClass.php"><button class="button button1">Classes</button></a>
+                            <a href="/StudentGrades.php"><button class="button button2">Grades</button></a> 
+                            <a href="/login.php"><button class="button button3" type=submit value=logout>Logout</button></a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+                  </body
+
 					
                 }
                  if (isParent($_SESSION)) 
@@ -63,3 +101,5 @@
 </div>
 	</body>
 </html>
+
+
